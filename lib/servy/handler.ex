c9a671b222a4d0e9452de42ed3e0573f188cafc3
Zachariah Ngonyani <zachariahn@gmail.com>
2020-02-28 06:06:26 +0300

    Add logging

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,8 +2,14 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
+    |> log
     |> route
     |> format_response
+  end
+
+  def log(conv) do
+    IO.inspect conv
+    conv
   end
 
   def parse(request) do
@@ -20,8 +26,6 @@ defmodule Servy.Handler do
   end
 
   def format_response(conv) do
-    # TODO: Use the values in the map to create an HTTP response string
-
     """
     HTTP/1.1 200 OK
     Content-Type: text/html
