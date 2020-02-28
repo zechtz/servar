42e346a5aa39ca30eb2652846ffdc4079128a7df
Zachariah Ngonyani <zachariahn@gmail.com>
2020-02-28 06:07:37 +0300

    Refactor the log function

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -7,10 +7,7 @@ defmodule Servy.Handler do
     |> format_response
   end
 
-  def log(conv) do
-    IO.inspect conv
-    conv
-  end
+  def log(conv),  do: IO.inspect conv
 
   def parse(request) do
     [method, path, _] =
