20074fa34f0ad0e1e720b1b631b5925d2a2da7fc
Zachariah Ngonyani <zachariahn@gmail.com>
2020-02-28 06:32:23 +0300

    Create helper function to return http status message

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -15,7 +15,11 @@ defmodule Servy.Handler do
       |> String.split("\n")
       |> List.first
       |> String.split(" ")
-    %{ method: method , path: path, resp_body: "" }
+    %{method: method ,
+      path: path,
+      status: nil,
+      resp_body: ""
+    }
   end
 
   def route(conv) do
@@ -23,25 +27,36 @@ defmodule Servy.Handler do
   end
 
   def route(conv, "GET", "/wildthings") do
-    %{ conv | resp_body: "Bears, Lions, Tigers" }
+    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
   end
 
   def route(conv, "GET", "/bears") do
-    %{ conv | resp_body: "Teddy, Smokey, Paddington" }
+    %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
   end
 
   def route(conv, _method, path) do
-    %{ conv | resp_body: "No #{path} found here" }
+    %{ conv | status: 404, resp_body: "No #{path} found here" }
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
 
     #{conv.resp_body}
     """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
   end
 end
 
