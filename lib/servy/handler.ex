11dbc8c432979e5a26f4d640586124bdfc976f86
Zachariah Ngonyani <zachariahn@gmail.com>
2020-02-28 09:23:44 +0300

    Add tracking and logging functions

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,10 +2,25 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
+    |> rewrite_path
     |> log
     |> route
+    |> track
     |> format_response
   end
+
+  def track(%{status: 404, path: path} = conv) do
+    IO.puts "Warning #{path} is on the loose!"
+    conv
+  end
+
+  def track(conv), do: conv
+
+  def rewrite_path(%{path: "/wildlife"} = conv) do
+    %{ conv | path: "/wildthings" }
+  end
+
+  def rewrite_path(conv), do: conv
 
   def log(conv),  do: IO.inspect conv
 
@@ -15,11 +30,7 @@ defmodule Servy.Handler do
       |> String.split("\n")
       |> List.first
       |> String.split(" ")
-    %{method: method ,
-      path: path,
-      status: nil,
-      resp_body: ""
-    }
+    %{method: method , path: path, status: nil, resp_body: "" }
   end
 
   def route(conv) do
@@ -32,6 +43,10 @@ defmodule Servy.Handler do
 
   def route(conv, "GET", "/bears") do
     %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
+  end
+
+  def route(conv, "GET", "/bears" <> id) do
+    %{ conv | status: 200, resp_body: "Bear #{id}" }
   end
 
   def route(conv, _method, path) do
@@ -83,7 +98,29 @@ response = Servy.Handler.handle(request)
 IO.puts(response)
 
 request = """
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts(response)
+
+request = """
 GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts(response)
+
+request = """
+GET /wildlife HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
