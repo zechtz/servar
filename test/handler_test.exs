92d0a926a29d89a63cedf560db4a5b9a3f46a503
Zachariah Ngonyani <zachariahn@gmail.com>
2020-03-02 02:03:01 +0300

    Make sure that all tests are green

--- test/handler_test.exs
+++ test/handler_test.exs
@@ -81,6 +81,30 @@ defmodule HandlerTest do
     assert remove_whitespace(response) == remove_whitespace(expected_response)
   end
 
+  test "POST /bears" do
+    request = """
+    POST /bears HTTP/1.1\r
+    Host: example.com\r
+    User-Agent: ExampleBrowser/1.0\r
+    Accept: */*\r
+    Content-Type: application/x-www-form-urlencoded\r
+    Content-Length: 21\r
+    \r
+    name=Baloo&type=Brown
+    """
+
+    response = handle(request)
+
+    assert response == """
+    HTTP/1.1 201 Created\r
+    Content-Type: text/html\r
+    Content-Length: 33\r
+    \r
+    Created a Brown bear named Baloo!
+    """
+  end
+
+
   test "GET /bigfoot" do
     request = """
     GET /bigfoot HTTP/1.1\r
