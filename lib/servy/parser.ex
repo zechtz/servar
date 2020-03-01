95aedf4bd0efdb41d7ddcf868e4ac0f9e99dcdaa
Zachariah Ngonyani <zachariahn@gmail.com>
2020-03-01 10:15:26 +0300

    Parse a request's query params

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -1,10 +1,35 @@
 defmodule Servy.Parser do
+
+  alias Servy.Conv, as: Conv
+
   def parse(request) do
-    [method, path, _] =
-      request
-      |> String.split("\n")
-      |> List.first
-      |> String.split(" ")
-    %{method: method , path: path, status: nil, resp_body: "" }
+    [top, params_string] = request |> String.split("\n\n")
+    [request_line | header_lines] = top |> String.split("\n")
+
+    [method, path, _] = request_line |> String.split(" ")
+
+    headers = parse_headers(header_lines, %{})
+    params = parse_params(headers["Content-Type"], params_string)
+
+    %Conv{
+      method: method ,
+      path: path,
+      params: params,
+      headers: headers
+    }
   end
+
+  def parse_params("application/x-www-form-urlencoded", params_string) do
+    params_string |> String.trim |> URI.decode_query
+  end
+
+  def parse_params(_, _), do: %{}
+
+  def parse_headers([head | tail], headers) do
+    [key, value] = String.split(head, ": ")
+    headers = Map.put(headers, key, value)
+    parse_headers(tail, headers)
+  end
+
+  def parse_headers([], headers), do: headers
 end
