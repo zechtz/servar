5111c7270fe3d97f9e6f9f9f52405ae3b791ab0d
Zachariah Ngonyani <zachariahn@gmail.com>
2020-03-01 14:21:48 +0300

    Refactor bear controller so data can be displayed on template

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -6,26 +6,32 @@ defmodule Servy.BearController do
   @templates_path Path.expand("../../public/templates", __DIR__)
 
   def index(conv) do
-    items =
+    bears =
       Wildthings.list_bears()
       |> Enum.sort(&Bear.order_asc_by_name/2)
 
     # TODO: Transform the bears into an HTML list
+    content =
+      @templates_path
+      |> Path.join("index.eex")
+      |> EEx.eval_file(bears: bears)
 
-    %{ conv | status: 201, resp_body: "#{items}" }
+    %{ conv | status: 201, resp_body: content }
   end
 
   def show(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
-    %{ conv | status: 200, resp_body: "<h1>Bear #{bear.id}: #{bear.name}"}
+
+    content =
+      @templates_path
+      |> Path.join("show.eex")
+      |> EEx.eval_file(bear: bear)
+
+    %{ conv | status: 200, resp_body: content}
   end
 
   def create(conv, %{"name" => name, "type" => type} = params) do
     IO.puts "Params: #{inspect(params)}"
     %{ conv | status: 201, resp_body: "Created a #{type} bear named #{name}!" }
-  end
-
-  defp bear_item(bear) do
-    "<li>#{bear.name} - #{bear.type}</li>"
   end
 end
