3d61644d2cc56a39eef7b7101a8539a97a98e2a0
Zachariah Ngonyani <zachariahn@gmail.com>
2020-03-01 14:35:19 +0300

    Refactor bear controller, use the render function to render templates

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -9,29 +9,25 @@ defmodule Servy.BearController do
     bears =
       Wildthings.list_bears()
       |> Enum.sort(&Bear.order_asc_by_name/2)
-
-    # TODO: Transform the bears into an HTML list
-    content =
-      @templates_path
-      |> Path.join("index.eex")
-      |> EEx.eval_file(bears: bears)
-
-    %{ conv | status: 201, resp_body: content }
+    render(conv, "index.eex", bears: bears)
   end
 
   def show(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
-
-    content =
-      @templates_path
-      |> Path.join("show.eex")
-      |> EEx.eval_file(bear: bear)
-
-    %{ conv | status: 200, resp_body: content}
+    render(conv, "show.eex", bear: bear)
   end
 
   def create(conv, %{"name" => name, "type" => type} = params) do
     IO.puts "Params: #{inspect(params)}"
     %{ conv | status: 201, resp_body: "Created a #{type} bear named #{name}!" }
+  end
+
+  defp render(conv, template, bindings \\ []) do
+    content =
+      @templates_path
+      |> Path.join(template)
+      |> EEx.eval_file(bindings)
+
+    %{ conv | status: 200, resp_body: content }
   end
 end
