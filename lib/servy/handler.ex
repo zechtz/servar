3ba28b1cdca7628c38696973d928ad68dd85c2d9
Zachariah Ngonyani <zachariahn@gmail.com>
2020-03-01 10:56:03 +0300

    Delegate handler functions to bear controller

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -6,7 +6,9 @@ defmodule Servy.Handler do
   import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
   import Servy.Parser, only: [parse: 1]
   import Servy.Conv, only: [full_status: 1]
+
   alias Servy.Conv, as: Conv
+  alias Servy.BearController
 
   @doc "Transforms a HTTP request into a HTTP response"
   def handle(request) do
@@ -24,20 +26,21 @@ defmodule Servy.Handler do
   #end
 
   def route(%Conv{method: "GET", path: "/wildthings"} = conv) do
-    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
+    BearController.index(conv)
   end
 
   def route(%Conv{method: "GET", path: "/bears"} = conv) do
-    %{ conv | status: 201, resp_body: "Teddy, Smokey, Paddington" }
+    BearController.index(conv)
   end
 
   # name=Baloo&type=Brown
   def route(%Conv{method: "POST", path: "/bears"} = conv) do
-    %{ conv | status: 201, resp_body: "Created a #{conv.params["type"]} bear named #{conv.params["name"]}!" }
+    BearController.create(conv, conv.params)
   end
 
   def route(%Conv{method: "GET", path: "/bears" <> id} = conv) do
-    %{ conv | status: 200, resp_body: "Bear #{id}" }
+    params = Map.put(conv.params, "id", id)
+    BearController.show(conv, params)
   end
 
   def route(%Conv{method: "GET", path: "/about"} = conv) do
