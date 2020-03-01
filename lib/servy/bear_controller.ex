326a2a4be1fc12574c76b320a29643ef8825888e
Zachariah Ngonyani <zachariahn@gmail.com>
2020-03-01 13:47:17 +0300

    Use the capture operator to invoke functions in bear controller

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -1,14 +1,34 @@
 defmodule Servy.BearController do
+
+  alias Servy.Wildthings
+  alias Servy.Bear
+
+  @templates_path Path.expand("../../public/templates", __DIR__)
+
   def index(conv) do
-    %{ conv | status: 201, resp_body: "Teddy, Smokey, Paddington" }
+    items =
+      Wildthings.list_bears()
+      |> Enum.filter(&Bear.is_grizzly/1)
+      |> Enum.sort(&Bear.order_asc_by_name/2)
+      |> Enum.map(&bear_item/1)
+      |> Enum.join
+
+    # TODO: Transform the bears into an HTML list
+
+    %{ conv | status: 201, resp_body: "#{items}" }
   end
 
   def show(conv, %{"id" => id}) do
-    %{ conv | status: 200, resp_body: "Bear #{id}" }
+    bear = Wildthings.get_bear(id)
+    %{ conv | status: 200, resp_body: "<h1>Bear #{bear.id}: #{bear.name}"}
   end
 
   def create(conv, %{"name" => name, "type" => type} = params) do
     IO.puts "Params: #{inspect(params)}"
     %{ conv | status: 201, resp_body: "Created a #{type} bear named #{name}!" }
+  end
+
+  defp bear_item(bear) do
+    "<li>#{bear.name} - #{bear.type}</li>"
   end
 end
