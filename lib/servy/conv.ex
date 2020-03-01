4632a2c0cbe8bdafe6817191ad607c20509b8ca5
Zachariah Ngonyani <zachariahn@gmail.com>
2020-03-01 05:11:51 +0300

    Refactor handler, move status_reason function to conv struct

--- lib/servy/conv.ex
+++ lib/servy/conv.ex
@@ -1,3 +1,18 @@
 defmodule Servy.Conv do
   defstruct method: "", path: "", status: nil, resp_body: ""
+
+  def full_status(conv) do
+    "#{conv.status} #{status_reason(conv.status)}"
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
 end
