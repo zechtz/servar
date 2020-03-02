a8319c8d81a5b6f7f49921ac7e7d9476e720c06a
Zachariah Ngonyani <zachariahn@gmail.com>
2020-03-02 03:41:43 +0300

    Add content type to Conv struct

--- lib/servy/conv.ex
+++ lib/servy/conv.ex
@@ -1,5 +1,5 @@
 defmodule Servy.Conv do
-  defstruct method: "", path: "", params: %{}, headers: %{}, status: nil, resp_body: ""
+  defstruct method: "", path: "", params: %{}, headers: %{}, resp_content_type: "text/html", status: nil, resp_body: ""
 
   def full_status(conv) do
     "#{conv.status} #{status_reason(conv.status)}"
