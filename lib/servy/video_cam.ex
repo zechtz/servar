0bac8b4c8740e71d3ff6e1327016371fef2f501b
Zachariah Ngonyani <zachariahn@gmail.com>
2020-03-02 12:17:35 +0300

    Add video cam module

---
+++ lib/servy/video_cam.ex
@@ -0,0 +1,15 @@
+defmodule Servy.VideoCam do
+  @doc """
+  Simulates sending a request to an external API
+  to get snapshot image from a video camera
+  """
+  def get_snapshot(camera_name) do
+    # CODE GOES HERE TO SEND A REQUEST TO AN EXTERNAL API
+    #
+    # sleep for one second to simulate that the API can be slow:
+    :timer.sleep(1000)
+
+    # example response returned from the API:
+    "#{camera_name}-snapshot.jpg"
+  end
+end
