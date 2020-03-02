aaeacddc902edd75ef8ad63ea8aa448a1073e622
Zachariah Ngonyani <zachariahn@gmail.com>
2020-03-02 09:15:38 +0300

    Finish http server second iteration

--- lib/servy/http_server.ex
+++ lib/servy/http_server.ex
@@ -1,19 +1,91 @@
-#server() ->
-  #{ok, LSock} = gen_tcp:listen(5678, [binary, {packet, 0}, {active, false}]),
-  #{ok, Sock} = gen_tcp:accept(LSock),
-  #{ok, Bin} = do_recv(Sock, []),
-  #ok = gen_tcp:close(Sock)
-  #Bin.
-# variables in Erlang starting with small letters are atoms in Elixir
-# while those starting with capital letters are just normal variables
-# calling erlang module from elixir you prefix with colon and use dot to call functions
-
 defmodule Servy.HttpServer do
-  def server do
-    {:ok, lsock} = :gen_tcp.listen(5678, [:binary, packet: 0, active: false])
-    {:ok, sock} = :gen_tcp.accept(lsock)
-    {:ok, bin} = :gen_tcp.recv(sock, 0)
-    :ok = :gen_tcp.close(sock)
-    bin
+
+  @doc """
+  starts the server n the given `port` of localhost
+  """
+  def start(port) when is_integer(port) and port > 1023 do
+    # Creates a socket to listen for client connections.
+    # `listen_socket` is bound to the listening socket
+    {:ok, listen_socket} = :gen_tcp.listen(port, [:binary, packet: :raw, active: false, reuseaddr: true])
+    #{:ok, lsock} = :gen_tcp.listen(5678, [:binary, packet: 0, active: false])
+
+    # Socket options (don't worry about these details):
+    # `:binary` - open the socket in "binary" mode and deliver data as binaries
+    # `packet: :raw` - deliver the entire binary without doing any packet handling
+    # `active: false` - receive data when we're ready by calling `:gen_tcp.recv/2`
+    # `reuseaddr: true` - allows reusing the address if the listener crushes
+
+    IO.puts "\nListening for connection requests on port #{port}"
+
+    accept_loop(listen_socket)
+  end
+
+  @doc """
+  Accepts client connections on the `listen_socket`.
+  """
+  def accept_loop(listen_socket) do
+    IO.puts "Waiting to accept a client connection...\n"
+
+    # Suspends(blocks) and waits for a client connection. When a connection
+    # is accepted `client_socket` is bound to a new client socket.
+    {:ok, client_socket} = :gen_tcp.accept(listen_socket)
+
+    IO.puts "Connection accepted!\n"
+
+    # Receives the request and sends a response over the client socket.
+    serve(client_socket)
+
+    # loop back and wait to accept the next connection
+    accept_loop(listen_socket)
+  end
+
+  @doc """
+  Receives the request n the `client_socket` and
+  sends a response back over the same socket.
+  """
+  def serve(client_socket) do
+    client_socket
+    |> read_request
+    |> Servy.Handler.handle
+    |> write_response(client_socket)
+  end
+
+  @doc """
+  Receives a request on the `client_socket`.
+  """
+  def read_request(client_socket) do
+    {:ok, request} = :gen_tcp.recv(client_socket, 0) # all available bytes
+
+    IO.puts "=> Received requests:\n"
+    IO.puts request
+
+    request
+  end
+
+  @doc """
+  Returns a generic HTTP response.
+  """
+  def generate_response(_request) do
+    """
+    HTTP/1.1 200 OK\r
+    Content-Type: text/plain\r
+    Content-Length: 6\r
+    \r
+    Hello
+    """
+  end
+
+  @doc """
+  Sends the `response` over the `client_socket`
+  """
+  def write_response(response, client_socket) do
+    :ok = :gen_tcp.send(client_socket, response)
+
+    IO.puts "<= Sent response\n"
+    IO.puts response
+
+    # closes the client socket, ending the connection.
+    # Does not close the listen socket!
+    :gen_tcp.close(client_socket)
   end
 end
