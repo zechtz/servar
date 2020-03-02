61eb1b440df1fd06d2d8b876fc18c54fb8dea8c8
Zachariah Ngonyani <zachariahn@gmail.com>
2020-03-02 03:40:09 +0300

    Remove sorting from api controller

--- lib/servy/api/bear_controller.ex
+++ lib/servy/api/bear_controller.ex
@@ -6,9 +6,8 @@ defmodule Servy.Api.BearController do
   def index(conv) do
     json =
       Wildthings.list_bears()
-      |> Enum.sort(&Bear.order_asc_by_name/2)
       |> Jason.encode!
 
-    %{ conv | status: 200, resp_content_type: "application/json", resp_body: json  }
+    %{ conv | status: 200, resp_content_type: "application/json",  resp_body: json  }
   end
 end
