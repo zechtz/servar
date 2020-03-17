7d5f165c6853cddd7ef08854956945ae13462425
Zachariah Ngonyani <zachariahn@gmail.com>
2020-03-17 06:55:52 +0300

    Create and refactor pledge server

---
+++ lib/servy/pledge_server.ex
@@ -0,0 +1,54 @@
+defmodule Servy.PledgeServer do
+
+  @name :pledge_server
+
+  def start do
+    IO.puts "\nStarting the pledge server..."
+    pid = spawn(__MODULE__, :listen_loop, [[]])
+    Process.register(pid, @name)
+    pid
+  end
+
+  def listen_loop(state) do
+    receive do
+      {sender, :create_pledge, name, amount} ->
+        {:ok, id} = send_pledge_to_service(name, amount)
+        new_state = [{name, amount} | state]
+        send sender, {:response, id}
+        listen_loop(new_state)
+      {sender, :recent_pledges} ->
+        send sender, {:response, state}
+        listen_loop(state)
+    end
+
+  end
+
+  def create_pledge(name, amount) do
+    send @name, {self(), :create_pledge, name, amount}
+
+    receive do {:response, status} -> status end
+  end
+
+  def recent_pledges do
+    send @name, {self(), :recent_pledges}
+
+    receive do {:response, pledges} -> pledges end
+  end
+
+
+  defp send_pledge_to_service(name, amount) do
+    # CODE GOES HERE TO SEND PLEDGE TO EXTERNAL SERVICE
+    {:ok, "pledge-#{:rand.uniform(1000)} by #{name} amount #{amount}"}
+  end
+end
+
+alias Servy.PledgeServer
+
+PledgeServer.start()
+
+IO.inspect PledgeServer.create_pledge("Mtabe", 1000)
+IO.inspect PledgeServer.create_pledge("Des", 1200)
+IO.inspect PledgeServer.create_pledge("Zach", 300)
+IO.inspect PledgeServer.create_pledge("Mtati", 500)
+
+IO.inspect PledgeServer.recent_pledges()
