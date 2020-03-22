db65b79454b252dc1b5dc3252b154bf7fc44da6f
Zachariah Ngonyani <zachariahn@gmail.com>
2020-03-22 09:31:33 +0300

    Refactor pledge server towards generic server implementation

---
+++ lib/servy/generic_server.ex
@@ -0,0 +1,33 @@
+defmodule Servy.GenericServer do
+
+  def start(callback_module, initial_state, name) do
+    pid = spawn(__MODULE__, :listen_loop, [initial_state, callback_module])
+    Process.register(pid, name)
+    pid
+  end
+
+  def call(pid, message) do
+    send pid, {:call, self(), message}
+
+    receive do {:response, response} -> response end
+  end
+
+  def cast(pid, message) do
+    send pid, {:cast, message}
+  end
+
+  def listen_loop(state, callback_module) do
+    receive do
+      {:call, sender, message} when is_pid(sender) ->
+        {response, new_state} = callback_module.handle_call(message, state)
+        send sender, {:response, response}
+        listen_loop(new_state, callback_module)
+      {:cast, message} ->
+        new_state = callback_module.handle_cast(message, state)
+        listen_loop(new_state, callback_module)
+      unexpected ->
+        IO.puts "Unexpected message #{inspect unexpected}"
+        listen_loop(state, callback_module)
+    end
+  end
+end
