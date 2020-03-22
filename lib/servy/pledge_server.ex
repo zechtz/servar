f149c7cb2f03cfd0670ffe357fa5f69424edb2f8
Zachariah Ngonyani <zachariahn@gmail.com>
2020-03-22 06:31:12 +0300

    Finish pledge server

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -2,25 +2,13 @@ defmodule Servy.PledgeServer do
 
   @name :pledge_server
 
+  # Client Interface
+
   def start do
     IO.puts "\nStarting the pledge server..."
     pid = spawn(__MODULE__, :listen_loop, [[]])
     Process.register(pid, @name)
     pid
-  end
-
-  def listen_loop(state) do
-    receive do
-      {sender, :create_pledge, name, amount} ->
-        {:ok, id} = send_pledge_to_service(name, amount)
-        new_state = [{name, amount} | state]
-        send sender, {:response, id}
-        listen_loop(new_state)
-      {sender, :recent_pledges} ->
-        send sender, {:response, state}
-        listen_loop(state)
-    end
-
   end
 
   def create_pledge(name, amount) do
@@ -35,6 +23,31 @@ defmodule Servy.PledgeServer do
     receive do {:response, pledges} -> pledges end
   end
 
+  def total_pledged do
+    send @name, {self(), :total_pledged}
+
+    receive do {:response, total} -> total end
+  end
+
+  # Server Interface
+
+  def listen_loop(state) do
+    receive do
+      {sender, :create_pledge, name, amount} ->
+        {:ok, id} = send_pledge_to_service(name, amount)
+        new_state = [{name, amount} | state]
+        send sender, {:response, id}
+        listen_loop(new_state)
+      {sender, :recent_pledges} ->
+        send sender, {:response, state}
+        listen_loop(state)
+      {sender, :total_pledged} ->
+        total = Enum.map(state, &elem(&1, 1)) |> Enum.sum
+        send sender, {:response, total}
+        listen_loop(state)
+    end
+
+  end
 
   defp send_pledge_to_service(name, amount) do
     # CODE GOES HERE TO SEND PLEDGE TO EXTERNAL SERVICE
@@ -42,13 +55,15 @@ defmodule Servy.PledgeServer do
   end
 end
 
-#alias Servy.PledgeServer
+alias Servy.PledgeServer
 
-#PledgeServer.start()
+PledgeServer.start()
 
-#IO.inspect PledgeServer.create_pledge("Mtabe", 1000)
-#IO.inspect PledgeServer.create_pledge("Des", 1200)
-#IO.inspect PledgeServer.create_pledge("Zach", 300)
-#IO.inspect PledgeServer.create_pledge("Mtati", 500)
+IO.inspect PledgeServer.create_pledge("Mtabe", 1000)
+IO.inspect PledgeServer.create_pledge("Des", 1200)
+IO.inspect PledgeServer.create_pledge("Zach", 300)
+IO.inspect PledgeServer.create_pledge("Mtati", 500)
 
-#IO.inspect PledgeServer.recent_pledges()
+IO.inspect PledgeServer.recent_pledges()
+
+IO.inspect PledgeServer.total_pledged()
